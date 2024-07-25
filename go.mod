0f47ef1621c09b60291095306790d3894c00b5c4
nicholasduplsumardi <musasigentakekura@gmail.com>
2024-07-25 16:52:28 +0700

    update total

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/nicholasduplsumardi/go-say-hello
+module github.com/nicholasduplsumardi/go-say-hello/v2
 
 go 1.22.1
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello()  string{
-	return "hello world asdadasdas"	
+func SayHello(name string)  string{
+	return "hello world asdadasdas"	+ name + "AHAHAHAHAH MAMPUS"
 }
